692fa86f25e080f31ec8bc799fcdf59d34efdfb1
Ujjal Satpathy <ujjalsatpathy@gmail.com>
2022-02-10 23:22:10 +0530

    Added cloud_event CF

--- cloud_functions/main.py
+++ cloud_functions/main.py
@@ -1,7 +1,13 @@
-def greet(requests):
-    req_args = requests.args
+import functions_framework
+import flask
+import requests
 
-    if req_args and "name" in req_args:
-        return f"Hello {req_args['name']}!"
-    else:
-        return "Sorry.No name provided!"
+
+def get_users(request):
+    users = requests.get('https://gorest.co.in/public/v2/users').json()
+    return flask.jsonify(users)
+
+
+@functions_framework.cloud_event
+def hello_cloud_event(cloud_event):
+    print(f"Received event with ID: {cloud_event['id']} and data {cloud_event.data}")
